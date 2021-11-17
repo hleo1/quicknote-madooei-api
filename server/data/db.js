cde2b19789b76c0e3fdd516404c1dba84ae7e519
hleo1 <leo.hubert3@gmail.com>
2021-11-17 12:39:09 -0500

    new uri

--- server/data/db.js
+++ server/data/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const URI = process.env.DB_URI;
+const URI = `mongodb+srv://quicknote-admin:V0yLoIcHN7EbWcby@quickname.rbqxg.mongodb.net/quickNoteDB?retryWrites=true&w=majority`;
 
 async function connect() {
   try {
